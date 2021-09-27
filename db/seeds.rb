84c48427af7e3d9e6436d9e877b66359e7b24919
Jenny Kim <kimj1905@gmail.com>
2021-09-27 15:17:56 -0400

    added primary seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+jenny = User.create(username: "jennyjellybeans")
+janedoe = User.create(username: "janedoe")
+
+fall = Collection.create(name: "Fall 2021", user_id: 1)
+vibes = Collection.create(name: "vibey illustrations", user_id: 1)
+doges = Collection.create(name: "Cute Doggos", user_id: 1)
+random = Collection.create(name: "Random Sample", user_id: 2)
+
