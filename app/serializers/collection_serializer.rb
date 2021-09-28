cca380c9bc0f7a83b6671232cd727817349d7fb0
Jenny Kim <kimj1905@gmail.com>
2021-09-28 16:37:30 -0400

    unsuccessfully tried creating custom attribute for collections serializer

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -1,4 +1,34 @@
 class CollectionSerializer < ActiveModel::Serializer
-  attributes :id, :name, :created_at
-  belongs_to   :user
+  attributes :id, :name, :created_at, :pictures
+  belongs_to :user
+
+  def pictures
+    # ActiveStorage::Blob.all
+    # ActiveStorage::Blob.select do |x|
+      Collection.all.each do |collection|
+        collection.pictures.all.each do |picture|
+          # debugger
+          # ActiveStorage::Blob.where(blob_id: picture.id)
+          ActiveStorage::Blob.find(picture.id)
+        end
+      end
+      # until (Collection.count + 1)
+
+      # x.id == 
+    # end
+    # ActiveStorage::Attachment.all
+    # ActiveStorage::Blob.all.where(ActiveStorage::Attachment.where(record_id:))
+    # Collection.all.each do |collection|
+      # id = collection.id
+      # ActiveStorage::Attachment.find_by_record_id(id)
+      # ActiveStorage::Blob.find_by_record_id(id)
+      # ActiveStorage::Attachment.where(record_id: collection.id)
+      # Find the blob id through record id through Attachment and get blob id to match Blob
+      # ActiveStorage::Attachment.select do |x|
+      #   x.record_id == id
+      # end
+    # end
+
+
+  end
 end
