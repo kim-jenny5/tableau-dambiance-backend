323074de7469f0fd0dd4111fb220b387ef3f3bff
Jenny Kim <kimj1905@gmail.com>
2021-09-29 18:17:10 -0400

    added create method and set up user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
         users = User.all
         render json: users
     end
+
+    def create
+        user = User.new(user_params)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
+    end
 end
