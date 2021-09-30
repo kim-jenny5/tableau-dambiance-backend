c0bf5a149eda1cc959f3ebe8e4f7feb6ad1fc79f
Jenny Kim <kimj1905@gmail.com>
2021-09-30 19:18:46 -0400

    added pictures url to picture api

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,12 @@
 class PicturesController < ApplicationController
     def index
         pictures = Picture.all
-        render json: pictures
+        render json: pictures.map { |img| ({ img_file: url_for(img) })}
     end
+
+    # def show
+    #     # byebug
+    #     picture = Picture.find(params[:id])
+    #     # render json: picture
+    # end
 end
