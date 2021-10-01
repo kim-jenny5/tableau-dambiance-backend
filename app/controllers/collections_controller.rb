7321710c5fbf4009d8ee474844420322107f4ad6
Jenny Kim <kimj1905@gmail.com>
2021-09-30 22:01:28 -0400

    set up create and destroy methods for collections and pictures

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -6,4 +6,18 @@ class CollectionsController < ApplicationController
         # render json: collections, include: { pictures.map { |img| ({ id: img.id, img_file: url_for(img) }) }}
         render json: collections
     end
+
+    def create
+        collection = Collection.new(collection_params)
+    end
+
+    def destroy
+        # collection = Collection
+    end
+
+    private
+    
+    def collection_params
+        params.require(:collection).permit(:name, :user_id)
+    end
 end
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,9 +4,23 @@ class PicturesController < ApplicationController
         render json: pictures.map { |img| ({ id: img.id, img_file: url_for(img), collection: img.collection })}
     end
 
-    # def show
-    #     # byebug
-    #     picture = Picture.find(params[:id])
-    #     # render json: picture
-    # end
+    def show
+        # byebug
+        picture = Picture.find(params[:id])
+        render json: picture
+    end
+
+    def create
+        picture = Picture.new(picture_params)
+    end
+
+    def destroy
+        
+    end
+
+    private
+    
+    def picture_params
+        # params.require(:picture).permit(:collection_id)
+    end
 end
