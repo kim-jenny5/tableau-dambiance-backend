7d39cde8b376b9bbc1b6e545a3c5601406c80eb6
Jenny Kim <kimj1905@gmail.com>
2021-10-05 19:17:55 -0400

    multiple 'untitled' collections will trigger numbering

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -7,7 +7,7 @@ class CollectionsController < ApplicationController
     def create
         collection = Collection.new(collection_params)
 
-        if collection.name.blank?
+        if collection.name.blank? || collection.name == "untitled"
             arr = collection.user.collections.select { |c| c.name.include?("untitled") }
             if arr.length > 0
                 collection.name = "untitled-#{arr.length}"
