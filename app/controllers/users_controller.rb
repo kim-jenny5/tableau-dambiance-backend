0f4f7fa5ab481468cd64091953dc00f0ede535f9
Jenny Kim <kimj1905@gmail.com>
2021-10-04 21:39:52 -0400

    added render json code to user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,12 @@ class UsersController < ApplicationController
 
     def create
         user = User.new(user_params)
+
+        if user.save
+            render json: user
+        else
+            render json: {error: "User couldn't be saved."}
+        end
     end
 
     private
