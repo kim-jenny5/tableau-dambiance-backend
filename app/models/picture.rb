e230210fafca68db9f07a441a8a898482e48128f
Jenny Kim <kimj1905@gmail.com>
2021-10-08 18:01:22 -0400

    got url to be coming from aws s3 not locally

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,8 @@
 class Picture < ApplicationRecord
   has_one_attached :img_file
   belongs_to :collection
+
+  def url
+    self.img_file.attachment.service_url
+  end
 end
--- app/serializers/picture_serializer.rb
+++ app/serializers/picture_serializer.rb
@@ -3,7 +3,9 @@ class PictureSerializer < ActiveModel::Serializer
   # attributes :id
   belongs_to :collection
 
-  def url
-    Rails.application.routes.url_helpers.rails_blob_path(object.img_file, object.signed_id, object.filename, only_path: true)
-  end
+  # def url
+  #   # Rails.application.routes.url_helpers.rails_blob_path(object.img_file, only_path: true)
+  #   # self.img_file.attachment.service_url
+  #   # Rails.application.routes.url_helpers.rails_blob_path(object.img_file, object.signed_id, object.filename, only_path: true)
+  # end
 end
