7c459a9c26e0ad6b3f9a448eb3673e30881bb315
Jenny Kim <kimj1905@gmail.com>
2021-10-18 22:15:16 -0400

    uncommented out seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,22 +14,22 @@ doges = Collection.create(name: "Cute Doggos", user_id: 1)
 random = Collection.create(name: "Random Sample", user_id: 2)
 
 
-# 8.times do |x|
-#     pic = Picture.create(collection_id: 1)
-#     pic.img_file.attach(io: File.open("/Users/yunjeonk/Documents/Flatiron/Javascript Project/Collection Pictures/fall/fall#{x+1}.jpeg"), filename: "fall#{x+1}.jpeg")
-# end
+8.times do |x|
+    pic = Picture.create(collection_id: 1)
+    pic.img_file.attach(io: File.open("/Users/yunjeonk/Documents/Flatiron/Javascript Project/Collection Pictures/fall/fall#{x+1}.jpeg"), filename: "fall#{x+1}.jpeg")
+end
 
-# 6.times do |x|
-#     pic = Picture.create(collection_id: 2)
-#     pic.img_file.attach(io: File.open("/Users/yunjeonk/Documents/Flatiron/Javascript Project/Collection Pictures/vibes/vibe#{x+1}.jpeg"), filename: "vibe#{x+1}.jpeg")
-# end
+6.times do |x|
+    pic = Picture.create(collection_id: 2)
+    pic.img_file.attach(io: File.open("/Users/yunjeonk/Documents/Flatiron/Javascript Project/Collection Pictures/vibes/vibe#{x+1}.jpeg"), filename: "vibe#{x+1}.jpeg")
+end
 
-# 6.times do |x|
-#     pic = Picture.create(collection_id: 3)
-#     pic.img_file.attach(io: File.open("/Users/yunjeonk/Documents/Flatiron/Javascript Project/Collection Pictures/dogs/dog#{x+1}.jpeg"), filename: "dog#{x+1}.jpeg")
-# end
+6.times do |x|
+    pic = Picture.create(collection_id: 3)
+    pic.img_file.attach(io: File.open("/Users/yunjeonk/Documents/Flatiron/Javascript Project/Collection Pictures/dogs/dog#{x+1}.jpeg"), filename: "dog#{x+1}.jpeg")
+end
 
-# 7.times do |x|
-#     pic = Picture.create(collection_id: 4)
-#     pic.img_file.attach(io: File.open("/Users/yunjeonk/Documents/Flatiron/Javascript Project/Collection Pictures/random/random#{x+1}.jpeg"), filename: "random#{x+1}.jpeg")
-# end
+7.times do |x|
+    pic = Picture.create(collection_id: 4)
+    pic.img_file.attach(io: File.open("/Users/yunjeonk/Documents/Flatiron/Javascript Project/Collection Pictures/random/random#{x+1}.jpeg"), filename: "random#{x+1}.jpeg")
+end
